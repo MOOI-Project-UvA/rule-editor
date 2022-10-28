433717b75e2ada17b0e29ad1e6e485a8cbb6c19c
Ioannis Tolios <ioannis.tolios@tno.nl>
2022-10-28 17:16:30 +0200

    updated source view: 1) added form for searching DB for legalTexts, added a sample legalText.

--- gui/src/views/FrameEditorView.vue
+++ gui/src/views/FrameEditorView.vue
@@ -1,43 +1,41 @@
 <template>
   <div id="frame-type-buttons" class="q-pa-md q-gutter-sm">
-      <q-btn
-        color="primary"
-        icon="mdi-file-document-edit-outline"
-        label="Add act frame"
-        @click="currentFrameType = 'act'"
-      />
-      <q-btn
-        color="secondary"
-        icon="mdi-file-document-edit-outline"
-        label="Add fact frame"
-        @click="currentFrameType = 'fact'"
-      />
-      <q-btn
-        color="purple"
-        icon="mdi-file-document-edit-outline"
-        label="Add duty frame"
-        @click="currentFrameType = 'duty'"
-      />
+    <q-btn
+      color="primary"
+      icon="mdi-file-document-edit-outline"
+      label="Add act frame"
+      @click="currentFrameType = 'act'"
+    />
+    <q-btn
+      color="secondary"
+      icon="mdi-file-document-edit-outline"
+      label="Add fact frame"
+      @click="currentFrameType = 'fact'"
+    />
+    <q-btn
+      color="purple"
+      icon="mdi-file-document-edit-outline"
+      label="Add duty frame"
+      @click="currentFrameType = 'duty'"
+    />
   </div>
   <div id="current-frame">
     <template v-if="currentFrameType == 'act'">
-      <ActFrameForm @closed="currentFrameType = null"/>
+      <ActFrameForm @closed="currentFrameType = null" />
     </template>
   </div>
 </template>
 
 <script>
-import ActFrameForm from '../components/ActFrameForm.vue'
+import ActFrameForm from "../components/ActFrameForm.vue";
 export default {
   data: () => ({
-    currentFrameType: null
+    currentFrameType: null,
   }),
   components: {
-    ActFrameForm
+    ActFrameForm,
   },
-
-}
+};
 </script>
 
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>
