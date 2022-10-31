54db5932fb1122ecbfa0e2c38fc6b4b776c02bd0
Erik <erik@bloomingdata.com>
2022-10-31 11:37:29 +0100

    Added forms for fact frame and duty frame

---
+++ gui/src/components/FactFrameForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-card flat bordered class="my-card">
+    <q-card-section>
+        <div class="text-h6">{{ frame.fact ? frame.fact : "Fact frame" }}</div>
+    </q-card-section>
+    <q-card-section class="q-pa-md q-gutter-sm">
+      <q-input square outlined clearable v-model="frame.fact" label="Fact" />
+      <q-input square outlined clearable v-model="frame.function" label="Function" />
+      <q-input square outlined clearable v-model="frame.source" label="Source" />
+    </q-card-section>
+    <q-card-actions>
+      <q-btn flat @click="cancelClicked">Cancel</q-btn>
+      <q-btn color="primary" @click="saveClicked">Save</q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    frame: {
+        type: 'fact',
+        fact: null,
+        function: null,
+        source: null
+    }
+  }),
+  methods: {
+    cancelClicked() {
+      this.$emit("closed")
+    },
+    saveClicked() {
+      //store frame
+      this.$store.commit("addFrame", this.frame)
+      this.$emit("closed")
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
