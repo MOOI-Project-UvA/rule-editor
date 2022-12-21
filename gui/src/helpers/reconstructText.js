f5a7411156978cfc0886dcb985fbbe7199082238
Ioannis Tolios <ioannis.tolios@tno.nl>
2022-12-21 18:40:04 +0100

    added some documentation

--- gui/src/helpers/reconstructText.js
+++ gui/src/helpers/reconstructText.js
@@ -1,3 +1,9 @@
+/*
+ * This function reconstructs a legal text from a decomposed source.
+ * @param  {String} text This is the string, where the reconstructed text will be stored
+ * @param  {Array}  dataForReconstruction An array of objects. Each object contains represents a decomposed part of the source.
+ * @return {String} The reconstructed text
+ */
 export default function reconstructText(text, dataForReconstruction) {
   for (const sectionIndex in dataForReconstruction) {
     const currentElement = dataForReconstruction[sectionIndex];
@@ -5,6 +11,7 @@ export default function reconstructText(text, dataForReconstruction) {
     // get the content based on type.
     // this part could be determined by the type of the ontology
     //TODO:determine the which object key should be used based on the ontology..
+    //TODO: determine if html or string should be returned
     if (currentElement.type === "Hoofdstuk") {
       text += `<p>${currentElement.label}</p>`;
     } else if (currentElement.type === "Artikel") {
