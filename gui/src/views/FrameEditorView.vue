ab097be77be068e485cb80fc32e1a76091da244e
Ioannis Tolios <ioannis.tolios@tno.nl>
2023-04-04 15:24:20 +0200

    updated the layout of the actions view

--- gui/src/views/FrameEditorView.vue
+++ gui/src/views/FrameEditorView.vue
@@ -1,11 +1,4 @@
 <template>
-  <div id="frame-type-buttons" class="q-pa-md q-gutter-sm">
-    Add:
-    <q-btn color="primary" :icon="icons['complexFact']" label="complex fact"
-      @click="$store.dispatch('createComplexFact')" />
-    <q-btn color="primary" :icon="icons['act']" label="act" @click="$store.dispatch('createAct')" />
-    <q-btn color="primary" :icon="icons['duty']" label="duty" @click="startNewFrame('duty')" disabled />
-  </div>
   <div id="current-frame" v-if="frameBeingEdited">
     <template v-if="frameBeingEdited.type == 'act'">
       <ActFrameForm @closed="closeActiveFrame" />
@@ -23,39 +16,34 @@
 </template>
 
 <script>
-import ActFrameForm from '../components/ActFrameForm.vue'
-import AtomicFactFrameForm from '../components/AtomicFactFrameForm.vue'
-import ComplexFactFrameForm from '../components/ComplexFactFrameForm.vue'
-import DutyFrameForm from '../components/DutyFrameForm.vue'
-import { icons, colors } from '../helpers/config.js'
+import ActFrameForm from "../components/ActFrameForm.vue";
+import AtomicFactFrameForm from "../components/AtomicFactFrameForm.vue";
+import ComplexFactFrameForm from "../components/ComplexFactFrameForm.vue";
+import DutyFrameForm from "../components/DutyFrameForm.vue";
 
 export default {
-  data: () => ({
-    icons: icons,
-    colors: colors
-  }),
   components: {
     ActFrameForm,
     AtomicFactFrameForm,
     ComplexFactFrameForm,
-    DutyFrameForm
+    DutyFrameForm,
   },
   computed: {
     activeFrameData() {
-      return this.$store.state.activeFrameData
+      return this.$store.state.activeFrameData;
     },
     frameBeingEdited() {
-      return this.$store.state.frameBeingEdited
-    }
+      return this.$store.state.frameBeingEdited;
+    },
   },
   methods: {
     startNewFrame(type) {
-      this.$store.dispatch("startNewFrame", type)
+      this.$store.dispatch("startNewFrame", type);
     },
     closeActiveFrame() {
-      this.$store.commit("setFrameBeingEdited", null)
-    }
-  }
+      this.$store.commit("setFrameBeingEdited", null);
+    },
+  },
 };
 </script>
 
