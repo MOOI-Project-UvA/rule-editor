f9a05833c5fadf19f5524ea3f758497e41295c25
Ioannis Tolios <itolios@ioanniss-mbp.tsn.tno.nl>
2023-08-15 13:45:27 +0200

    added reconstruction of legal from source

--- gui/src/helpers/ontologyServices.js
+++ gui/src/helpers/ontologyServices.js
@@ -1,4 +1,4 @@
-import { Namespace, parse, serialize } from "rdflib"
+import { Namespace, parse, serialize } from "rdflib";
 
 const rdfConfig = {
   docUri: "http://ontology.tno.nl/normengineering/choppr",
@@ -11,50 +11,50 @@ const rdfConfig = {
     XML: Namespace("http://www.w3.org/XML/1998/namespace"),
     XSD: Namespace("http://www.w3.org/2001/XMLSchema#"),
     SRC: Namespace("http://ontology.tno.nl/normengineering/source#"),
-    NE: Namespace("http://ontology.tno.nl/normengineering/")
-  }
-}
+    NE: Namespace("http://ontology.tno.nl/normengineering/"),
+  },
+};
 
 function jsonld2turtle(jsonString, store, uri) {
-  return new Promise(resolve => {
-    parse(jsonString, store, uri, "application/ld+json", (e,s) => {
+  return new Promise((resolve) => {
+    parse(jsonString, store, uri, "application/ld+json", (e, s) => {
       if (e) {
-        console.log("Parse Error! ")
-        return resolve(e)
-      }
-      else {
-        return resolve(s)
+        console.log("Parse Error! ");
+        return resolve(e);
+      } else {
+        return resolve(s);
       }
 
       // by omitting the document parameter to serialize, or passing null, we will get all the triples in the store.
       serialize(null, store, uri, "text/turtle", (e, s) => {
         if (e) {
-          console.log("Serialize Error!")
-          return resolve(e)
+          console.log("Serialize Error!");
+          return resolve(e);
         }
         // console.log("sss", s, resolve(s));
-        return resolve(s)
-      })
-    })
-  })
+        return resolve(s);
+      });
+    });
+  });
 }
 
-function parseInterpretation(string, store,uri, contentType="application/ld+json") {
-  return new Promise(resolve => {
-
+function parseInterpretation(
+  string,
+  store,
+  uri,
+  contentType = "application/ld+json",
+) {
+  return new Promise((resolve) => {
     parse(string, store, uri, "application/ld+json", (e, s) => {
       if (e) {
-        console.log("Parse Error! ")
-        return resolve(e)
+        console.log("Parse Error! ");
+        return resolve(e);
       } else {
-        return resolve(s)
+        return resolve(s);
       }
-
-    })
-  })
+    });
+  });
 }
-
-
 
 // root -> collection.elements
 /*
@@ -63,21 +63,110 @@ function parseInterpretation(string, store,uri, contentType="application/ld+json
  * It returns the reconstructed string
  * Extensions: add support for multiple sources
  */
-function reconstructTextFromRdf(root,store){
-
-  const hasContent = store.statementsMatching(root, rdfConfig.namespaces.SRC('hasContent'), null)
-  let content = hasContent.length > 0 ? `<p>${hasContent[0].object.value}</p>` : ``
-  const hasChildren = store.statementsMatching(root, rdfConfig.namespaces.SRC('hasChildElementOrdering'), null)
+function reconstructTextFromRdf(root, store) {
+  const hasContent = store.statementsMatching(
+    root,
+    rdfConfig.namespaces.SRC("hasContent"),
+    null,
+  );
+  // console.log("hasContent:", hasContent)
+  let content =
+    hasContent.length > 0 ? `<p>${hasContent[0].object.value}</p>` : ``;
+  const hasChildren = store.statementsMatching(
+    root,
+    rdfConfig.namespaces.SRC("hasChildElementOrdering"),
+    null,
+  );
+  // console.log("hasChildren:", hasChildren)
   // let children = hasChildren.length> 0 ? hasChildren
-  if (hasChildren.length>0) {
-      hasChildren[0].object.elements.forEach(c => {
-          content = content.concat(reconstructTextFromRdf(c, store))
-      })
+  if (hasChildren.length > 0) {
+    hasChildren[0].object.elements.forEach((c) => {
+      content = content.concat(reconstructTextFromRdf(c, store));
+    });
   }
-  return content
+  return content;
 }
 
-  // return html`${reconstructTextFromRdf(docObject[0].subject)}`
+/**
+ * Reconstructs a legal source from JSON-LD
+ * @param {Object} root The named Node of the document to be reconstructed
+ * @param  store The rdf graph, where the document will be store in the form of triples
+ * @returns {Array|*[]} An array of objects containing information per source element. The order of the objects
+ * corresponds to the proper order of elements in the source.
+ */
+function reconstructDataStructureFromRDF(root, store) {
+  console.log("store in function", store);
+  const hasContent = store.statementsMatching(
+    root,
+    rdfConfig.namespaces.SRC("hasContent"),
+    null,
+  );
+  let sentences =
+    hasContent.length > 0 ? sentenceData(hasContent, root, store) : [];
+  const hasChildren = store.statementsMatching(
+    root,
+    rdfConfig.namespaces.SRC("hasChildElementOrdering"),
+    null,
+  );
+  if (hasChildren.length > 0) {
+    hasChildren[0].object.elements.forEach((c) => {
+      sentences = sentences.concat(reconstructDataStructureFromRDF(c, store));
+    });
+  }
+  console.log("sentences: ", sentences);
+  return sentences;
+}
 
+/**
+ * Creates an object per element with the following information:
+ * 1) the id of the chunk,
+ * 2) the type of the element,
+ * 3) the text related to the chunk
+ * For more info, check the RDFLIb.js library ()
+ * @param {Array} hasContent An array containing the triple referring to the NamedNode of the element to be examined
+ * @param {Object} root The named RDF node of the element to be examined
+ * @param {Object} store THe RDF graph containing the triples for the document in question
+ * @return {Array} An array containing the created object per element
+ */
+function sentenceData(hasContent, root, store) {
+  const structure = { id: "", content: "", annotations: [], type: "" };
+  structure.content = hasContent[0].object.value;
+  structure.id = getElementsId(root, store);
+  structure.type = store.any(
+    root,
+    rdfConfig.namespaces.SRC("hasTypeLabel"),
+    null,
+  ).value;
+  return [structure];
+}
 
-export { rdfConfig, jsonld2turtle, parseInterpretation, reconstructTextFromRdf }
+/**
+ * Retrieves the id of an element from the rdf store
+ * @param {Object} root The named RDF node of the element to be examined
+ * @param {Object} store THe RDF graph containing the triples for the document in question
+ * @returns {string}
+ */
+function getElementsId(root, store) {
+  const result = store.statementsMatching(
+    root,
+    rdfConfig.namespaces.SRC("hasTypeLabel"),
+    null,
+  );
+  // const object = store.any(root, namespaces.SRC("hasTypeLabel"),null)
+  console.log("root", root);
+  // console.log("object", object)
+  if (result[0].object.value === "Document") {
+    return result[0].subject.value;
+  } else {
+    const segments = result[0].subject.value.split("/");
+    return segments[segments.length - 1].split("_")[0];
+  }
+}
+
+export {
+  rdfConfig,
+  jsonld2turtle,
+  parseInterpretation,
+  reconstructTextFromRdf,
+  reconstructDataStructureFromRDF,
+};
--- gui/src/store/index.js
+++ gui/src/store/index.js
@@ -1,14 +1,23 @@
 import { createStore } from "vuex";
 import { Fact } from "../model/fact.js";
-import { Act, ClaimDuty } from "../helpers/flint.js"
+import { Act, ClaimDuty } from "../helpers/flint.js";
 import reconstructText from "../helpers/reconstructText.js";
 import { saveAs } from "file-saver";
 import { parseJsonToFrames } from "../helpers/import.js";
-import { json, text } from 'd3-fetch'
-import { addParentReferencesToDocument, getSentencesInDocument } from "../helpers/document.js"
-import { v4 as uuid4 } from 'uuid'
-import {rdfConfig, jsonld2turtle, parseInterpretation, reconstructTextFromRdf} from "../helpers/ontologyServices.js";
-import * as rdf from "rdflib"
+import { json, text } from "d3-fetch";
+import {
+  addParentReferencesToDocument,
+  getSentencesInDocument,
+} from "../helpers/document.js";
+import { v4 as uuid4 } from "uuid";
+import {
+  rdfConfig,
+  jsonld2turtle,
+  parseInterpretation,
+  reconstructTextFromRdf,
+  reconstructDataStructureFromRDF,
+} from "../helpers/ontologyServices.js";
+import * as rdf from "rdflib";
 // import { parseInterpretation } from "../helpers/ontologyServices.js";
 
 // Create a new store instance.
@@ -32,48 +41,48 @@ const store = createStore({
       availableSources: [], //list of sources that the user can choose from
       rdfStore: rdf.graph(),
       docUri: "http://ontology.tno.nl/normengineering/choppr",
-      jsonString: ""
+      jsonString: "",
     };
   },
   getters: {
     getFileContent: (state) => state.fileContent,
     reconstructedData: (state) => state.reconstructedData,
-    rdfStore: (state)=> state.rdfStore
+    rdfStore: (state) => state.rdfStore,
   },
   mutations: {
     addFrame(state, frame) {
       //add frame if it does not exist yet
-      if (!(state.frames.includes(frame))) {
+      if (!state.frames.includes(frame)) {
         //set unique id for this frame
         frame["id"] = uuid4();
         state.frames = [...state.frames, frame];
       }
     },
     addNewFrame(state, { frameType, annotation }) {
-      let frame
+      let frame;
       switch (frameType.class) {
-        case 'fact':
-          frame = new Fact()
+        case "fact":
+          frame = new Fact();
           if (annotation) {
-            frame.addAnnotation(annotation) //this also sets annotation.frame
+            frame.addAnnotation(annotation); //this also sets annotation.frame
           }
           break;
-        case 'relation':
+        case "relation":
           switch (frameType.id) {
-            case 'act':
-              frame = new Act()
+            case "act":
+              frame = new Act();
               break;
-            case 'claim_duty':
-              frame = new ClaimDuty()
+            case "claim_duty":
+              frame = new ClaimDuty();
               break;
           }
           break;
       }
-      frame.type = frameType
+      frame.type = frameType;
       frame["id"] = uuid4();
       state.frames = [...state.frames, frame];
-      state.frameBeingEdited = frame
-      console.log("frameBeingEdited", frame)
+      state.frameBeingEdited = frame;
+      console.log("frameBeingEdited", frame);
     },
     // setAnnotationMode(state, selectedMode) {
     //   state.annotationMode = selectedMode;
@@ -97,32 +106,34 @@ const store = createStore({
     //   console.log("reconstuct")
     // },
     addAnnotation(state, annotation) {
-      console.log("adding annotation", annotation)
+      console.log("adding annotation", annotation);
       state.annotations = [...state.annotations, annotation];
     },
     removeAnnotation(state, annotation) {
-      console.log("removeAnnotations")
-      const index = state.annotations.indexOf(annotation)
+      console.log("removeAnnotations");
+      const index = state.annotations.indexOf(annotation);
       if (index != -1) {
-        state.annotations.splice(index, 1)
-        console.log("state.annotations", state.annotations)
+        state.annotations.splice(index, 1);
+        console.log("state.annotations", state.annotations);
       }
     },
     setAnnotationBeingEdited(state, annotation) {
-      state.annotationBeingEdited = annotation
+      state.annotationBeingEdited = annotation;
     },
     removeFrame(state, frame) {
-      const index = state.frames.indexOf(frame)
-      state.frames.splice(index, 1)
+      const index = state.frames.indexOf(frame);
+      state.frames.splice(index, 1);
     },
     removeAtomicFact(state, frame) {
       // remove the fact from an act or a complexFact
       // case1: complex fact
       // get the generated ids (frame._id) of the complexFrames, which contain the AtomicFact
 
-      state.frames.filter(f => f.type == 'fact' && f.booleanConstruct).forEach(f => {
-        f.booleanConstruct.removeFrame(frame)
-      })
+      state.frames
+        .filter((f) => f.type == "fact" && f.booleanConstruct)
+        .forEach((f) => {
+          f.booleanConstruct.removeFrame(frame);
+        });
       // const complexFramesIds = state.frames
       //   .filter((fr) => fr._type === "complexFact")
       //   .filter((fr) => fr._factList.find((d) => d._id === frame._id))
@@ -188,31 +199,31 @@ const store = createStore({
             index
           ]._terminates.filter((fr) => fr._id !== frame._id);
           state.frames[index]._creates = state.frames[index]._creates.filter(
-            (fr) => fr._id !== frame._id
+            (fr) => fr._id !== frame._id,
           );
           state.frames[index]._action =
             state.frames[index]._action !== null &&
-              state.frames[index]._action._id === frame._id
+            state.frames[index]._action._id === frame._id
               ? null
               : state.frames[index]._action;
           state.frames[index]._actor =
             state.frames[index]._actor !== null &&
-              state.frames[index]._actor._id == frame._id
+            state.frames[index]._actor._id == frame._id
               ? null
               : state.frames[index]._actor;
           state.frames[index]._object =
             state.frames[index]._object !== null &&
-              state.frames[index]._object._id == frame._id
+            state.frames[index]._object._id == frame._id
               ? null
               : state.frames[index]._object;
           state.frames[index]._precondition =
             state.frames[index]._precondition !== null &&
-              state.frames[index]._precondition._id == frame._id
+            state.frames[index]._precondition._id == frame._id
               ? null
               : state.frames[index]._precondition;
           state.frames[index]._recipient =
             state.frames[index]._recipient !== null &&
-              state.frames[index]._recipient._id == frame._id
+            state.frames[index]._recipient._id == frame._id
               ? null
               : state.frames[index]._recipient;
         });
@@ -222,73 +233,77 @@ const store = createStore({
       state.frames = state.frames.filter((fr) => fr._id !== frame._id);
       // console.log("updated list of frames:", state.frames.length, state.frames);
     },
-
   },
   actions: {
     readAvailableSources(context) {
-      console.log("reading available sources")
-      json('/sources.json').then(data => {
-        context.state.availableSources = data
-      })
+      console.log("reading available sources");
+      json("/sources.json").then((data) => {
+        context.state.availableSources = data;
+      });
     },
     //reads source, so user can annotate and create frames
     //source object contains filename where to read the source from
     async addSource(context, sourceId) {
       // console.log("addSource", sourceId)
-      const source = this.state.availableSources.find(s => s.id == sourceId)
-      console.log("source", source)
-      console.log("context.state: ", context.state.rdfStore)
-      await json(source.fileName).then(data=>{
+      const source = this.state.availableSources.find((s) => s.id == sourceId);
 
-        console.log("data", data)
-        context.state.jsonString = JSON.stringify(data)
-         // console.log("string", context.state.jsonString)
-         const document = data['@graph'].find(d => 'document' in d).document
-        document.title = source.title
-        console.log("document", document)
-        //add parent references to each part of the document
-        addParentReferencesToDocument(document)
-        console.log("afterAddParentRefs", document)
-        // add attribute to each sentence to store annotations
-        getSentencesInDocument(document).forEach(s => s['annotations'] = [])
-
-
-      })
-      console.log("after:", context.state.jsonString)
-       // await rdf.parse(context.state.jsonString, context.state.rdfStore, context.state.docUri, "application/ld+json")
-      const resp = await parseInterpretation(context.state.jsonString,context.state.rdfStore, context.state.docUri)
-      // rdf.parse(context.state.jsonString, context.state.rdfStore, context.state.docUri, "application/ld+json")
-      console.log("resp", resp)
-      console.log("statements: ", context.state.rdfStore.statementsMatching(null,null,null).length)
-      const docObject = context.state.rdfStore.statementsMatching(null, rdfConfig.namespaces.RDF('type'), rdfConfig.namespaces.SRC('Document') )
-      console.log("docObject", docObject)
-      reconstructTextFromRdf(docObject[0].subject,context.state.rdfStore)
-      context.state.sourceDocuments = [...context.state.sourceDocuments, document]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // const document = data['@graph'].find(d => 'document' in d).document
-        // document.title = source.title
-        // console.log("document", document)
-        // //add parent references to each part of the document
+      let docData = {
+        title: "",
+        children: [],
+        id: "",
+      };
+      await json(source.fileName).then((data) => {
+        console.log("data", data);
+        context.state.jsonString = JSON.stringify(data);
+        // console.log("string", context.state.jsonString)
+        const document = data["@graph"].find((d) => "document" in d).document;
+        document.title = source.title;
+        docData.title = source.title;
+        console.log("document", document);
+        // add parent references to each part of the document
         // addParentReferencesToDocument(document)
         // console.log("afterAddParentRefs", document)
-        //add attribute to each sentence to store annotations
-        // getSentencesInDocument(document).forEach(s => s['annotations'] = [])
-        // context.state.sourceDocuments = [...context.state.sourceDocuments, document]
-        // console.log("context.state.sourceDocuments", context.state.sourceDocuments)
+        // add attribute to each sentence to store annotations
+        getSentencesInDocument(document).forEach(
+          (s) => (s["annotations"] = []),
+        );
+        console.log("sentences:", getSentencesInDocument(document));
+      });
+
+      const resp = await parseInterpretation(
+        context.state.jsonString,
+        context.state.rdfStore,
+        context.state.docUri,
+      );
+
+      const docObject = context.state.rdfStore.statementsMatching(
+        null,
+        rdfConfig.namespaces.RDF("type"),
+        rdfConfig.namespaces.SRC("Document"),
+      );
+
+      docData.children = reconstructDataStructureFromRDF(
+        docObject[0].subject,
+        context.state.rdfStore,
+      );
+      // TODO: create a structure with queries similar to the following:
+      context.state.sourceDocuments = [
+        ...context.state.sourceDocuments,
+        docData,
+      ];
+
+      // await rdf.parse(context.state.jsonString, context.state.rdfStore, context.state.docUri, "application/ld+json")
+
+      // const document = data['@graph'].find(d => 'document' in d).document
+      // document.title = source.title
+      // console.log("document", document)
+      // //add parent references to each part of the document
+      // addParentReferencesToDocument(document)
+      // console.log("afterAddParentRefs", document)
+      //add attribute to each sentence to store annotations
+      // getSentencesInDocument(document).forEach(s => s['annotations'] = [])
+      // context.state.sourceDocuments = [...context.state.sourceDocuments, document]
+      // console.log("context.state.sourceDocuments", context.state.sourceDocuments)
       // })
     },
     createAct(context) {
@@ -296,12 +311,14 @@ const store = createStore({
       context.state.frameBeingEdited = new Act();
     },
     saveInterpretation(context) {
-      console.log("saving interpretation")
+      console.log("saving interpretation");
       //convert frames to json string
       //replace object references by id's
-      console.log("frames", context.state.frames)
-      const string = JSON.stringify(context.state.frames.map(f => f.toFlatObject()))
-      console.log("string", string)
+      console.log("frames", context.state.frames);
+      const string = JSON.stringify(
+        context.state.frames.map((f) => f.toFlatObject()),
+      );
+      console.log("string", string);
       const blob = new Blob([string], {
         type: "text/plain;charset=utf-8",
       });
@@ -309,50 +326,68 @@ const store = createStore({
       saveAs(blob, `${dateString}_interpretation.json`);
     },
     loadInterpretation(context, jsonText) {
-      context.state.frames = parseJsonToFrames(jsonText)
-      console.log("loaded interpretation", context.state.frames)
+      context.state.frames = parseJsonToFrames(jsonText);
+      console.log("loaded interpretation", context.state.frames);
     },
     // gets the id of the hovered frame
     // and updates the frames array, which contain
     highlightElements(context, hoveredElement) {
-
       // array with ids of the related elements ...
-      let relatedIds = new Array(hoveredElement._id)
+      let relatedIds = new Array(hoveredElement._id);
       // Case 1: Hovering over an atomic fact, higlight the related parents
-      if (hoveredElement._type === "fact" && !hoveredElement._booleanConstruct) {
+      if (
+        hoveredElement._type === "fact" &&
+        !hoveredElement._booleanConstruct
+      ) {
         // check if acts contain this element
-        context.state.frames.filter(d => d._type === 'act')
-          .forEach(d => d.checkFrameExistance(d, hoveredElement) ? relatedIds.push(d._id) : null)
+        context.state.frames
+          .filter((d) => d._type === "act")
+          .forEach((d) =>
+            d.checkFrameExistance(d, hoveredElement)
+              ? relatedIds.push(d._id)
+              : null,
+          );
         // console.log("actIds without contexts:", relatedIds)
 
         // check if contexts contain this element
-        context.state.frames.filter(d => d._type === 'fact' && d._booleanConstruct && d._id !== hoveredElement._id)
-          .forEach(d => d.checkFrameExistance(hoveredElement) ? relatedIds.push(d._id) : null)
+        context.state.frames
+          .filter(
+            (d) =>
+              d._type === "fact" &&
+              d._booleanConstruct &&
+              d._id !== hoveredElement._id,
+          )
+          .forEach((d) =>
+            d.checkFrameExistance(hoveredElement)
+              ? relatedIds.push(d._id)
+              : null,
+          );
         // console.log("actIds with contexts:", relatedIds)
         // console.log("context.state.frames: ", context.state.frames)
       }
       // Case 2: if the hovered element is an Act, highlight the corresponding facts
       if (hoveredElement._type === "act") {
-        relatedIds = relatedIds.concat(hoveredElement.childrenIds)
+        relatedIds = relatedIds.concat(hoveredElement.childrenIds);
       }
       // Case 3: if the hovered element is a context, highlight the corresponding facts
       // check if a booleanConstruct could contain acts or other complex structures.
       if (hoveredElement._type === "fact" && hoveredElement._booleanConstruct) {
         // give me all the children
-        relatedIds = relatedIds.concat(hoveredElement.retrieveChildrenIds)
+        relatedIds = relatedIds.concat(hoveredElement.retrieveChildrenIds);
       }
 
       // change the transparecy of the non-related atomic facts
       context.state.frames.forEach((d) => {
-        return relatedIds.includes(d._id) ? d._highlight = false : d._highlight = true;
+        return relatedIds.includes(d._id)
+          ? (d._highlight = false)
+          : (d._highlight = true);
       });
     },
     // Mouseout restore
     unhighlightElements(context) {
-      context.state.frames.forEach(d => d._highlight = false);
-    }
-
-  }
+      context.state.frames.forEach((d) => (d._highlight = false));
+    },
+  },
 });
 
 export { store };
