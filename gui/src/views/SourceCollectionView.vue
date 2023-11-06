69ca9f550d1a8ae4d7d793bae0e8feac5cac4cb0
Ioannis Tolios <itolios@Ioanniss-MBP.home>
2023-11-07 00:28:37 +0100

    updated the layout. On the left the user can see the sources and on the right they get the list of sentences.

--- gui/src/views/SourceCollectionView.vue
+++ gui/src/views/SourceCollectionView.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- This component contains the content of the source collection view -->
   <div id="source-collection-view">
-    <q-card flat bordered class="my-card q-ma-sm" style="width: 600px">
+    <q-card flat bordered class="my-card q-ma-sm" style="width: 1000px">
       <!-- header -->
       <q-item class="q-ma-sm">
         <q-item-section avatar>
@@ -37,38 +37,113 @@
       </q-card-section>
       <div id="source-collection-card-content">
         <!-- the retrieved legal text will be shown here -->
-        <div v-for="(sourceDocument, docIndex) in sourceDocuments">
-          <q-card-section>
-            <q-list bordered class="rounded-borders q-pa-md">
-              <q-expansion-item
-                v-model="expandedSources[docIndex]"
-                expand-icon-toggle
-                switch-toggle-side
-                expand-separator
-                icon="mdi-book-search-outline"
-                :caption="sourceDocument.title"
-                default-opened
+        <div>
+          <q-splitter v-model="splitterModel" class="q-ma-lg">
+            <template v-slot:before>
+              <q-tabs v-model="tab" vertical class="text-teal" shrink>
+                <q-tab
+                  v-for="(sourceDocument, docIndex) in sourceDocuments"
+                  :key="docIndex"
+                  :name="docIndex"
+                  icon="mdi-book-outline"
+                  :label="sourceDocument.title"
+                />
+              </q-tabs>
+            </template>
+            <template v-slot:after>
+              <q-tab-panels
+                v-model="tab"
+                animated
+                swipeable
+                vertical
+                transition-prev="jump-up"
+                transition-next="jump-up"
               >
-                <q-card
-                  flat
-                  square
-                  class="q-ma-sm q-pa-sm expansion-items"
-                  :style="`max-height: calc(100vh - 136px - 78px - 88px - 48px - 24px - 72px - (${
-                    docIndex + 1
-                  } * 120px));`"
+                <q-tab-panel
+                  v-for="(sourceDocument, docIndex) in sourceDocuments"
+                  :key="docIndex"
+                  :name="docIndex"
                 >
-                  <q-card-section class="q-pt-none">
-                    <!-- show recursively all text leafs in the document tree -->
-                    <ListComponent
-                      :textPiece="sourceDocument.sentences"
-                      :docId="docIndex"
-                    />
-                  </q-card-section>
-                </q-card>
-              </q-expansion-item>
-            </q-list>
-          </q-card-section>
+                  <q-card flat square>
+                    <q-card-section class="q-pt-none expansion-items">
+                      <!-- show recursively all text leafs in the document tree -->
+                      <ListComponent
+                        :textPiece="sourceDocument.sentences"
+                        :docId="docIndex"
+                      />
+                    </q-card-section>
+                  </q-card>
+                </q-tab-panel>
+              </q-tab-panels>
+            </template>
+          </q-splitter>
+
+          <!--            <template v-slot:after>-->
+          <!--              <q-tab-panels-->
+          <!--                v-model="tab"-->
+          <!--                animated-->
+          <!--                swipeable-->
+          <!--                vertical-->
+          <!--                transition-prev="jump-up"-->
+          <!--                transition-next="jump-up"-->
+          <!--              >-->
+          <!--                <q-tab-panel-->
+          <!--                  v-for="(sourceDocument, docIndex) in sourceDocuments"-->
+          <!--                  :key="sourceDocument.title"-->
+          <!--                  v-bind="sourceDocument.title"-->
+          <!--                  :name="sourceDocument.title"-->
+          <!--                  icon="mdi-book-search-outline"-->
+          <!--                >-->
+          <!--                  <div class="text-h4 q-mb-md">{{ sourceDocument.title }}</div>-->
+          <!--                  <p>-->
+          <!--                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.-->
+          <!--                    Quis praesentium cumque magnam odio iure quidem, quod illum-->
+          <!--                    numquam possimus obcaecati commodi minima assumenda-->
+          <!--                    consectetur culpa fuga nulla ullam. In, libero.-->
+          <!--                  </p>-->
+          <!--                  <p>-->
+          <!--                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.-->
+          <!--                    Quis praesentium cumque magnam odio iure quidem, quod illum-->
+          <!--                    numquam possimus obcaecati commodi minima assumenda-->
+          <!--                    consectetur culpa fuga nulla ullam. In, libero.-->
+          <!--                  </p>-->
+          <!--                </q-tab-panel>-->
+          <!--              </q-tab-panels>-->
+          <!--            </template>-->
         </div>
+
+        <!--        <div v-for="(sourceDocument, docIndex) in sourceDocuments">-->
+        <!--          <q-card-section>-->
+        <!--            <q-list bordered class="rounded-borders q-pa-md">-->
+        <!--              <q-expansion-item-->
+        <!--                v-model="expandedSources[docIndex]"-->
+        <!--                expand-icon-toggle-->
+        <!--                switch-toggle-side-->
+        <!--                expand-separator-->
+        <!--                icon="mdi-book-search-outline"-->
+        <!--                :caption="sourceDocument.title"-->
+        <!--                default-opened-->
+        <!--              >-->
+        <!--                <q-card-->
+        <!--                  flat-->
+        <!--                  square-->
+        <!--                  class="q-ma-sm q-pa-sm expansion-items"-->
+        <!--                  :style="`max-height: calc(100vh - 136px - 78px - 88px - 48px - 24px - 72px - (${-->
+        <!--                    docIndex + 1-->
+        <!--                  } * 120px));`"-->
+        <!--                >-->
+        <!--                  <q-card-section class="q-pt-none">-->
+        <!--                    &lt;!&ndash; show recursively all text leafs in the document tree &ndash;&gt;-->
+        <!--                    <ListComponent-->
+        <!--                      :textPiece="sourceDocument.sentences"-->
+        <!--                      :docId="docIndex"-->
+        <!--                    />-->
+        <!--                  </q-card-section>-->
+        <!--                </q-card>-->
+        <!--              </q-expansion-item>-->
+        <!--            </q-list>-->
+        <!--          </q-card-section>-->
+        <!--        </div>-->
       </div>
       <!--  action section  -->
       <q-card-actions class="q-pa-md" id="source-collection-view-actions">
@@ -99,6 +174,8 @@ export default {
   },
   data: () => ({
     expandedSources: [],
+    splitterModel: 30,
+    tab: 0,
   }),
   computed: {
     sourceDocuments() {
@@ -124,11 +201,13 @@ export default {
       // emit event to the parent component to update the stepper
       this.$emit("updateStepper");
     },
+    setTabs: function () {},
   },
   watch: {
     sourceDocuments() {
       console.log(
         "sourceDocuments",
+        this.sourceDocuments,
         this.sourceDocuments
           .map((d) => d.sentences)
           .filter((d) => {
@@ -152,8 +231,8 @@ export default {
 #source-collection-card-content {
   //height: calc(100vh - 136px - 78px - 88px - 48px - 24px);
   height: calc(100vh - 136px - 78px - 88px - 48px - 24px);
-  //overflow-y: auto;
-  overflow: hidden;
+  overflow-y: hidden;
+  //overflow: hidden;
   z-index: 1 !important;
 }
 
@@ -168,6 +247,8 @@ export default {
 
 .expansion-items {
   //max-height: calc(100vh - 136px - 78px - 88px - 48px - 24px - 72px);
+  max-height: calc(100vh - 136px - 78px - 88px - 48px - 24px - 150px);
+  //height: calc(100vh - 136px - 78px - 88px - 48px - 24px - 72px);
   overflow-y: auto;
 }
 </style>
