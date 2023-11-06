d104735cd62cc6202e257c5549f39e708335dbeb
Ioannis Tolios <itolios@ioanniss-mbp.home.tsn.tno.nl>
2023-11-06 20:20:36 +0100

    removed the parent container(q-card) in the interpretation view

--- gui/src/views/InterpretationView.vue
+++ gui/src/views/InterpretationView.vue
@@ -19,66 +19,36 @@ export default {
 
 <template>
   <div id="interpretation-view">
-    <q-card flat bordered class="my-card q-ma-sm fill-height">
-      <!-- header -->
-      <q-item class="q-ma-sm">
-        <q-item-section avatar>
-          <q-avatar icon="mdi-thought-bubble-outline" rounded size="xl">
-          </q-avatar>
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Interpret sources</q-item-label>
-          <q-item-label caption>Step 3</q-item-label>
-        </q-item-section>
-        <q-item-section avatar>
-          <q-avatar>
-            <q-icon
-              name="mdi-information-outline"
-              class="cursor-pointer"
-            ></q-icon>
-            <q-tooltip class="bg-blue-1 text-grey-10 text-body2">
-              <div style="max-width: 300px">
-                In this view, you can interpret selected legal sources from the
-                repository. You are able to start the interpretation process by
-                selecting text snippets and assigning one of the four supported
-                labels: 1) Agent, 2) Action, 3) Object, 4) Conditions.
-              </div>
-            </q-tooltip>
-          </q-avatar>
-        </q-item-section>
-      </q-item>
-      <q-separator />
-      <!-- main content of the card -->
-      <!-- source view column -->
-      <div class="row q-mt-xl q-mx-lg">
-        <div class="col-4">
-          <SourceView />
+    <!-- main content of the card -->
+    <!-- source view column -->
+    <div class="row q-mt-xl q-mx-lg">
+      <div class="col-4">
+        <SourceView />
+      </div>
+      <div class="col-3">
+        <FrameNetworkView />
+      </div>
+      <!-- chip view + editor forms column-->
+      <div class="col-5">
+        <!-- save and load interpretation buttons -->
+        <div class="row">
+          <Menu />
         </div>
-        <div class="col-3">
-          <FrameNetworkView />
-        </div>
-        <!-- chip view + editor forms column-->
-        <div class="col-5">
-          <!-- save and load interpretation buttons -->
-          <div class="row">
-            <Menu />
-          </div>
-          <div class="row">
-            <!-- frame editor view -->
-            <FrameEditorView />
-          </div>
+        <div class="row">
+          <!-- frame editor view -->
+          <FrameEditorView />
         </div>
       </div>
-    </q-card>
+    </div>
   </div>
   <AnnotationPanel />
 </template>
 
 <style scoped lang="css">
 #interpretation-view {
-  min-height: calc(100vh - 72px - 48px - 50px);
+  //min-height: calc(100vh - 72px - 48px - 50px);
 }
 .fill-height {
-  min-height: calc(100vh - 72px - 48px - 50px);
+  //min-height: calc(100vh - 72px - 48px - 50px);
 }
 </style>
