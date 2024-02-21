63fc4b8dfe14841bd57faebbe1cc991bf3a10c2c
Ioannis Tolios <itolios@ioanniss-mbp.home.tsn.tno.nl>
2024-02-21 17:17:52 +0100

    created an alertWidget

---
+++ gui/src/helpers/alertWidget.js
@@ -0,0 +1,35 @@
+import { Notify } from "quasar";
+
+export const alertWidget = (type, message) => {
+  switch (type) {
+    case "error":
+      Notify.create({
+        message: message,
+        color: "negative",
+        icon: "mdi-alert-circle-outline",
+        position: "top",
+        actions: [
+          {
+            label: "Dismiss",
+            color: "white",
+          },
+        ],
+      });
+      break;
+
+    case "success":
+      Notify.create({
+        message: message,
+        color: "teal",
+        icon: "mdi-check-circle-outline",
+        position: "top",
+        actions: [
+          {
+            label: "Dismiss",
+            color: "white",
+          },
+        ],
+      });
+      break;
+  }
+};
