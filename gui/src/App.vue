ec0d2b00d2181f781578aae3c4834dd273387dba
Ioannis Tolios <itolios@ioanniss-mbp.home.tsn.tno.nl>
2024-04-24 12:24:40 +0200

    made the load/save buttons globally accessible. Related issue: #140.

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -18,28 +18,80 @@
   */
   -->
 
-
-  <q-stepper id="stepper-id" v-model="step" ref="stepper" color="primary" animated flat header-nav>
-    <q-step :name="1" title="Define a task" icon="mdi-head-dots-horizontal-outline" :done="step > 1" done-color="green"
-      caption="Step 1" class="row justify-center content-center" :header-nav="step > 1">
+  <q-stepper
+    id="stepper-id"
+    v-model="step"
+    ref="stepper"
+    color="primary"
+    animated
+    flat
+    header-nav
+  >
+    <q-step
+      :name="1"
+      title="Define a task"
+      icon="mdi-head-dots-horizontal-outline"
+      :done="step > 1"
+      done-color="green"
+      caption="Step 1"
+      class="row justify-center content-center"
+      :header-nav="step > 1"
+    >
       <TaskDefinitionView @update-stepper="updateStepperValue" />
     </q-step>
-    <q-step :name="2" title="Collect sources" icon="mdi-bookmark-box-multiple-outline"
-      class="row justify-center content-start" :done="step > 2" done-color="green" caption="Step 2"
-      :header-nav="step > 2">
-      <SourceCollectionView @update-stepper="updateStepperValue" @decrease-stepper="decreaseStepperValue" />
+    <q-step
+      :name="2"
+      title="Collect sources"
+      icon="mdi-bookmark-box-multiple-outline"
+      class="row justify-center content-start"
+      :done="step > 2"
+      done-color="green"
+      caption="Step 2"
+      :header-nav="step > 2"
+    >
+      <SourceCollectionView
+        @update-stepper="updateStepperValue"
+        @decrease-stepper="decreaseStepperValue"
+      />
     </q-step>
 
-    <q-step :name="3" title="Interpret sources" icon="mdi-thought-bubble-outline" :done="step > 3" done-color="green"
-      caption="Step 3" :header-nav="step > 3">
+    <q-step
+      :name="3"
+      title="Interpret sources"
+      icon="mdi-thought-bubble-outline"
+      :done="step > 3"
+      done-color="green"
+      caption="Step 3"
+      :header-nav="step > 3"
+    >
       <InterpretationView />
       <!-- <TestView/> -->
     </q-step>
-    <q-step :name="4" title="Validate interpretations" icon="mdi-timeline-check-outline" :done="step > 4" disable
-      caption="Step 4">
+    <q-step
+      :name="4"
+      title="Validate interpretations"
+      icon="mdi-timeline-check-outline"
+      :done="step > 4"
+      disable
+      caption="Step 4"
+    >
     </q-step>
-    <q-step :name="5" title="Perform task" icon="mdi-playlist-check" :done="step > 5" disable caption="Step 5">
+    <q-step
+      :name="5"
+      title="Perform task"
+      icon="mdi-playlist-check"
+      :done="step > 5"
+      disable
+      caption="Step 5"
+    >
     </q-step>
+
+    <!--  adding message slot, thus making the save/load interpretation tasks always accessible  -->
+    <template v-slot:message>
+      <q-banner class="bg-secondary text-white q-px-lg q-py-sm">
+        <load-save-interpretation-banner></load-save-interpretation-banner>
+      </q-banner>
+    </template>
   </q-stepper>
 </template>
 
@@ -47,6 +99,7 @@
 import TaskDefinitionView from "./views/TaskDefinitionView.vue";
 import SourceCollectionView from "./views/SourceCollectionView.vue";
 import InterpretationView from "./views/InterpretationView.vue";
+import LoadSaveInterpretationBanner from "./components/LoadSaveIntepretationBanner.vue";
 
 export default {
   name: "app",
@@ -55,9 +108,10 @@ export default {
   }),
 
   components: {
+    LoadSaveInterpretationBanner,
     InterpretationView,
     SourceCollectionView,
-    TaskDefinitionView
+    TaskDefinitionView,
   },
 
   mounted() {
