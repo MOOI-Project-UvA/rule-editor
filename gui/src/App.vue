48e3978bd4e85b289bec7dd9d43b85aa8158e849
Ioannis Tolios <itolios@ioanniss-mbp.tsn.tno.nl>
2024-05-22 17:07:59 +0200

    reactivated the information panel with version info

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -18,33 +18,80 @@
   */
   -->
 
-  <q-stepper id="stepper-id" v-model="this.$store.state.step" ref="stepper" color="primary" animated flat header-nav>
-    <q-step :name="1" title="Define a task" icon="mdi-head-dots-horizontal-outline" :done="this.$store.state.step > 1"
-      done-color="green" caption="Step 1" class="row justify-center content-center"
-      :header-nav="this.$store.state.step > 1">
+  <q-stepper
+    id="stepper-id"
+    v-model="this.$store.state.step"
+    ref="stepper"
+    color="primary"
+    animated
+    flat
+    header-nav
+  >
+    <q-step
+      :name="1"
+      title="Define a task"
+      icon="mdi-head-dots-horizontal-outline"
+      :done="this.$store.state.step > 1"
+      done-color="green"
+      caption="Step 1"
+      class="row justify-center content-center"
+      :header-nav="this.$store.state.step > 1"
+    >
       <TaskDefinitionView @update-stepper="updateStepperValue" />
     </q-step>
-    <q-step :name="2" title="Collect sources" icon="mdi-bookmark-box-multiple-outline"
-      class="row justify-center content-start" :done="this.$store.state.step > 2" done-color="green" caption="Step 2"
-      :header-nav="this.$store.state.step > 2">
-      <SourceCollectionView @update-stepper="updateStepperValue" @decrease-stepper="decreaseStepperValue" />
+    <q-step
+      :name="2"
+      title="Collect sources"
+      icon="mdi-bookmark-box-multiple-outline"
+      class="row justify-center content-start"
+      :done="this.$store.state.step > 2"
+      done-color="green"
+      caption="Step 2"
+      :header-nav="this.$store.state.step > 2"
+    >
+      <SourceCollectionView
+        @update-stepper="updateStepperValue"
+        @decrease-stepper="decreaseStepperValue"
+      />
     </q-step>
 
-    <q-step :name="3" title="Interpret sources" icon="mdi-thought-bubble-outline" :done="this.$store.state.step > 3"
-      done-color="green" caption="Step 3" :header-nav="this.$store.state.step > 3">
+    <q-step
+      :name="3"
+      title="Interpret sources"
+      icon="mdi-thought-bubble-outline"
+      :done="this.$store.state.step > 3"
+      done-color="green"
+      caption="Step 3"
+      :header-nav="this.$store.state.step > 3"
+    >
       <InterpretationView />
       <!-- <TestView/> -->
     </q-step>
-    <q-step :name="4" title="Validate interpretations" icon="mdi-timeline-check-outline"
-      :done="this.$store.state.step > 4" disable caption="Step 4">
+    <q-step
+      :name="4"
+      title="Validate interpretations"
+      icon="mdi-timeline-check-outline"
+      :done="this.$store.state.step > 4"
+      disable
+      caption="Step 4"
+    >
     </q-step>
-    <q-step :name="5" title="Perform task" icon="mdi-playlist-check" :done="this.$store.state.step > 5" disable
-      caption="Step 5">
+    <q-step
+      :name="5"
+      title="Perform task"
+      icon="mdi-playlist-check"
+      :done="this.$store.state.step > 5"
+      disable
+      caption="Step 5"
+    >
     </q-step>
 
     <!--  adding message slot, thus making the save/load interpretation tasks always accessible  -->
     <template v-slot:message>
-      <q-banner class="bg-blue-grey-8 text-white q-px-lg" style="padding-top: 1px; padding-bottom: 1px">
+      <q-banner
+        class="bg-blue-grey-8 text-white q-px-lg"
+        style="padding-top: 1px; padding-bottom: 1px"
+      >
         <load-save-interpretation-banner></load-save-interpretation-banner>
       </q-banner>
     </template>
@@ -56,6 +103,7 @@ import TaskDefinitionView from "./views/TaskDefinitionView.vue";
 import SourceCollectionView from "./views/SourceCollectionView.vue";
 import InterpretationView from "./views/InterpretationView.vue";
 import LoadSaveInterpretationBanner from "./components/LoadSaveIntepretationBanner.vue";
+import { alertWidget } from "./helpers/alertWidget.js";
 export default {
   name: "app",
   data: () => ({
@@ -63,11 +111,6 @@ export default {
     repo: import.meta.env.VITE_REPOSITORY_URL,
     branch: import.meta.env.VITE_BRANCH,
   }),
-
-  mounted() {
-    this.$store.state.step = 1
-  },
-
   components: {
     LoadSaveInterpretationBanner,
     InterpretationView,
@@ -79,18 +122,21 @@ export default {
     //FOR DEBUGGING EDITOR GUI, SKIP FIRST STEPS
     // this.$store.state.step = 3
     // this.$store.dispatch("loadInterpretationForDebugging")
-    // const urlToRender = `https://${this.repo.split(":").join("/")}/-/tree/${this.branch
-    //   }`;
-    // const commitUrl = `https://${this.repo.split(":").join("/")}/-/commit/${this.hash
-    //   }`;
-    // const message = `Welcome to the Norm editor! This version is based on the <a href='${urlToRender}' target='_blank'>${this.branch
-    //   }</a> branch.
-    // <br/>Commit hash: <a href='${commitUrl}' target='_blank'>${this.hash.substring(
-    //     0,
-    //     9,
-    //   )}</a>.`;
+    const urlToRender = `https://${this.repo.split(":").join("/")}/-/tree/${
+      this.branch
+    }`;
+    const commitUrl = `https://${this.repo.split(":").join("/")}/-/commit/${
+      this.hash
+    }`;
+    const message = `Welcome to the Norm editor! This version is based on the <a href='${urlToRender}' target='_blank'>${
+      this.branch
+    }</a> branch.
+    <br/>Commit hash: <a href='${commitUrl}' target='_blank'>${this.hash.substring(
+      0,
+      9,
+    )}</a>.`;
 
-    // alertWidget("welcome", message);
+    alertWidget("welcome", message);
     this.$store.dispatch("readAvailableSources");
   },
   methods: {
