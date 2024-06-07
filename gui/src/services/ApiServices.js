982b38d4c7ddf7f7f08000ba9a8fe93d17d2e2df
Ioannis Tolios <itolios@ioanniss-mbp.home.tsn.tno.nl>
2024-06-07 18:30:52 +0200

    added action in store for saving as ttl and added request

--- gui/src/services/ApiServices.js
+++ gui/src/services/ApiServices.js
@@ -1,26 +1,60 @@
-export default {
-  async fetchNlpPrediction(text) {
-    try {
-      const response = await fetch("/api/predict", {
+export async function fetchNlpPrediction(text) {
+  try {
+    const response = await fetch("/api/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": import.meta.env.VITE_X_API_KEY,
+      },
+      body: JSON.stringify({ text }),
+    });
+    console.log("response:", response);
+    if (!response.ok) {
+      throw new Error("An error occurred during the prediction request.");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(
+      "An error occurred during the prediction request: " + error.message,
+    );
+    return error;
+  }
+}
+
+  /*
+  Converts the JSON structure supported by the editor to RDF
+ */
+export async function convertToRDF(dataset) {
+  try {
+    const response = await fetch(
+      "http://localhost:5000/process_and_save",
+      // "/api/wrap/process_and_send",
+
+      {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           "X-API-KEY": import.meta.env.VITE_X_API_KEY,
         },
-        body: JSON.stringify({ text }),
-      });
-      console.log("response:", response);
-      if (!response.ok) {
-        throw new Error("An error occurred during the prediction request.");
-      }
+        body: dataset,
+      },
+    );
 
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      throw new Error(
-        "An error occurred during the prediction request: " + error.message,
-      );
-      return error;
+    if (!response.ok) {
+      throw new Error("An error occurred during sending the data.");
     }
-  },
-};
+
+    const data = await response.text();
+    console.log("response:", data);
+    return data;
+  } catch (error) {
+    throw new Error(
+      "An error occurred while converting data to rdf: " + error.message,
+    );
+    return error;
+  }
+}
+
+
