0f1bb03afcb0679c170e732040fbe0ee84a4b3b1
Erik <erik@bloomingdata.com>
2024-07-11 11:45:24 +0200

    Move load and save buttons to top

--- gui/src/components/LoadSaveIntepretationBanner.vue
+++ gui/src/components/LoadSaveIntepretationBanner.vue
@@ -31,88 +31,69 @@ export default {
 <template>
   <!-- banner container questions to save or load interpretations -->
 
-  <div class="row inline justify-start items-baseline no-wrap q-mb-sm q-mt-sm">
-    <div class="btn-area">
-      Would you like to
-      <q-btn
-        outline
-        class="q-mx-sm text-lowercase text-white"
-        label="save"
-        icon="mdi-content-save"
-      >
-        <q-menu fit transition-show="jump-down" transition-hide="jump-up">
-          <q-list>
-            <q-item class="label" disable>
-              <q-item-label>Locally</q-item-label>
-            </q-item>
-            <q-separator></q-separator>
-            <q-item
-              clickable
-              v-close-popup
-              dense
-              @click="saveInterpretationAsJson"
-            >
-              <q-item-section>JSON</q-item-section>
-            </q-item>
-            <q-item
-              clickable
-              v-close-popup
-              dense
-              @click="saveInterpretationAsTurtle"
-            >
-              <q-item-section>RDF</q-item-section>
-            </q-item>
-            <q-separator></q-separator>
-            <q-item class="label" disable>
-              <q-item-label>Remotely</q-item-label>
-            </q-item>
-            <q-separator></q-separator>
-            <q-item clickable v-close-popup dense disable>
-              <q-item-section>Triply</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
-      your progress or to
-      <q-btn
-        outline
-        class="q-mx-sm text-lowercase text-white"
-        label="load"
-        icon="mdi-file-upload-outline"
-      >
-        <q-menu fit transition-show="jump-down" transition-hide="jump-up">
-          <q-list>
-            <q-item class="label" disable>
-              <q-item-label>Locally</q-item-label>
-            </q-item>
-            <q-separator></q-separator>
-            <q-item clickable v-close-popup dense @click="chooseFile">
-              <q-item-section>JSON</q-item-section>
-            </q-item>
-            <q-item disable clickable v-close-popup dense>
-              <q-item-section>RDF</q-item-section>
-            </q-item>
-            <q-separator></q-separator>
-            <q-item class="label" disable>
-              <q-item-label>Remotely</q-item-label>
-            </q-item>
-            <q-separator></q-separator>
-            <q-item clickable v-close-popup dense disable>
-              <q-item-section>Triply</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
+  <div class="row q-gutter-sm q-ma-sm">
+    <q-btn round size="sm" icon="mdi-file-upload-outline" color="primary">
+      <q-menu fit transition-show="jump-down" transition-hide="jump-up">
+        <q-list>
+          <q-item class="label" disable>
+            <q-item-label>Locally</q-item-label>
+          </q-item>
+          <q-separator></q-separator>
+          <q-item clickable v-close-popup dense @click="chooseFile">
+            <q-item-section>JSON</q-item-section>
+          </q-item>
+          <q-item disable clickable v-close-popup dense>
+            <q-item-section>RDF</q-item-section>
+          </q-item>
+          <q-separator></q-separator>
+          <q-item class="label" disable>
+            <q-item-label>Remotely</q-item-label>
+          </q-item>
+          <q-separator></q-separator>
+          <q-item clickable v-close-popup dense disable>
+            <q-item-section>Triply</q-item-section>
+          </q-item>
+        </q-list>
+      </q-menu>
+      <q-tooltip class="bg-blue-1 text-grey-10 text-body2">
+        <div>
+          Load an interpretation
+        </div>
+      </q-tooltip>
+    </q-btn>
+    <q-btn round size="sm" icon="mdi-content-save" color="primary">
+      <q-menu fit transition-show="jump-down" transition-hide="jump-up">
+        <q-list>
+          <q-item class="label" disable>
+            <q-item-label>Locally</q-item-label>
+          </q-item>
+          <q-separator></q-separator>
+          <q-item clickable v-close-popup dense @click="saveInterpretationAsJson">
+            <q-item-section>JSON</q-item-section>
+          </q-item>
+          <q-item clickable v-close-popup dense @click="saveInterpretationAsTurtle">
+            <q-item-section>RDF</q-item-section>
+          </q-item>
+          <q-separator></q-separator>
+          <q-item class="label" disable>
+            <q-item-label>Remotely</q-item-label>
+          </q-item>
+          <q-separator></q-separator>
+          <q-item clickable v-close-popup dense disable>
+            <q-item-section>Triply</q-item-section>
+          </q-item>
+        </q-list>
+      </q-menu>
+      <q-tooltip class="bg-blue-1 text-grey-10 text-body2">
+        <div>
+          Save the current interpretation
+        </div>
+      </q-tooltip>
+    </q-btn>
 
-      <input
-        id="fileUpload"
-        type="file"
-        @change="handleFileSelection"
-        hidden
-        ref="fileUpload"
-      />
-      previous work of yours?
-    </div>
+
+    <input id="fileUpload" type="file" @change="handleFileSelection" hidden ref="fileUpload" />
+
   </div>
 </template>
 
