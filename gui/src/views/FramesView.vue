4cb2b258a3d18f0c6eda4b1da469da4b4fed2446
Erik <erik@bloomingdata.com>
2024-07-11 13:29:27 +0200

    Add buttons for switching between list and network view

--- gui/src/views/FramesView.vue
+++ gui/src/views/FramesView.vue
@@ -3,20 +3,30 @@
     <!-- card title section -->
     <div :class="sourceViewIsCollapsed ? 'container-row' : 'container-column'">
       <div :class="{ 'height-fill-available': !sourceViewIsCollapsed }">
-        <div class="height-content row q-pa-sm items-center">
+        <div class="height-content row q-pa-sm items-center q-gutter-md">
           <div class="col-1 text-bold">Frames</div>
-          <div class="col q-ml-md">
+          <div class="col-auto">
+            <div class="row q-gutter-xs items-center">
+              <div>View as:</div>
+              <div class="q-gutter-xs">
+                <q-radio size="xs" dense v-model="view" val="list" label="List" />
+                <q-radio size="xs" dense v-model="view" val="network" label="Network" />
+              </div>
+            </div>
+          </div>
+          <div class="col-auto">
             <NewFrameMenu />
           </div>
-
-          <q-input bottom-slots v-model="searchTerm" label="Filter frames on label" dense>
-            <template v-slot:prepend>
-              <q-icon name="mdi-magnify" />
-            </template>
-            <template v-slot:append>
-              <q-icon size="xs" name="mdi-close" @click="searchTerm = ''" class="cursor-pointer" />
-            </template>
-          </q-input>
+          <div class="col">
+            <q-input bottom-slots v-model="searchTerm" label="Filter frames on label" dense>
+              <template v-slot:prepend>
+                <q-icon name="mdi-magnify" />
+              </template>
+              <template v-slot:append>
+                <q-icon size="xs" name="mdi-close" @click="searchTerm = ''" class="cursor-pointer" />
+              </template>
+            </q-input>
+          </div>
           <div class="col-1">
             <q-avatar class="float-right" size="lg">
               <q-icon name="mdi-information-outline" class="cursor-pointer"></q-icon>
@@ -52,7 +62,8 @@ export default {
   data: () => ({
     icons: icons,
     colors: colors,
-    searchTerm: ""
+    searchTerm: "",
+    view: "list" //either 'list' or 'network'
   }),
   computed: {
     sourceViewIsCollapsed() {
