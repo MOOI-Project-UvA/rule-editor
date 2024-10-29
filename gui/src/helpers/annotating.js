966ee57d0903d344c004495b7889daf8ea16ceb2
Erik <erik@bloomingdata.com>
2024-10-29 14:24:26 +0100

    Prevent empty snippet when selecting to end of existing snipopet

--- gui/src/helpers/annotating.js
+++ gui/src/helpers/annotating.js
@@ -168,7 +168,7 @@ function splitSnippet(snippet, charIndex, sentence) {
     console.log("splitSnippet", snippet, charIndex, sentence)
     //If charIndex == 0 or charIndex == snippet.text.length-1 then snippet is not split, return the
     //original snippet
-    if (charIndex == 0 || charIndex == snippet.text.length - 1) {
+    if (charIndex == 0 || charIndex == snippet.text.length) {
         return [snippet]
     } else {
         const leftSnippet = new Snippet(sentence, [snippet.characterRange[0], snippet.characterRange[0] + charIndex])
