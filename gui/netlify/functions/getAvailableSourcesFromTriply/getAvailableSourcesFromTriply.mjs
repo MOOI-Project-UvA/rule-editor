43b296d3e734abab2aa6ab6646dfeef655174c54
itolios <ioannis.tolios@tno.nl>
2024-12-07 00:16:32 +0100

    added netlify configuration

---
+++ gui/netlify/functions/getAvailableSourcesFromTriply/getAvailableSourcesFromTriply.mjs
@@ -0,0 +1,34 @@
+// Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
+import SuperAgent from "superagent";
+
+
+exports.handler = async function (event, context) {
+  const token = process.env.TRIPLY_KEY;
+  const endpoint = process.env.TRIPLY_ENDPOINT
+   const reply = await SuperAgent.post(endpoint)
+    .set('Accept', 'application/sparql-results+json')
+    .set('Authorization', 'Bearer ' + token)
+    .buffer(true)
+    .send({
+      query: `
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX src: <http://ontology.tno.nl/normengineering/source#>
+      SELECT  ?iri ?title ?date ?editor WHERE {
+        ?iri a src:Source .
+        ?iri src:hasTitle ?title .
+        ?iri src:editedBy ?editoriri .
+        ?event src:generates ?iri ;
+          src:ends ?date .
+        ?editoriri rdfs:label ?editor .
+      } ORDER BY DESC(?date)
+      `
+    })
+    .accept('json')
+
+  console.log("source list", reply.body)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Sources retrieved!', sources: reply.body }),
+  };
+};
