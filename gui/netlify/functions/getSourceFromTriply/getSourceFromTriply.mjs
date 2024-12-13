f5523ae64491c39a11fe18c6e1de7be8ea8e2144
itolios <ioannis.tolios@tno.nl>
2024-12-13 01:41:01 +0100

    updated the getSourceFromTriply function. It gets a ttl string instead of file.

--- gui/netlify/functions/getSourceFromTriply/getSourceFromTriply.mjs
+++ gui/netlify/functions/getSourceFromTriply/getSourceFromTriply.mjs
@@ -1,20 +1,61 @@
 import App from "@triply/triplydb";
+import N3 from 'n3'
+
 
 export const handler = async function (event,context){
-    console.log("event: ", event.body)
-    console.log("context", context)
     const iri = JSON.parse(event.body)
+    console.log("iri:", iri)
     const token = process.env.TRIPLY_KEY;
+
+    const prefixes = {
+    calc: 'http://ontology.tno.nl/normengineering/calculemus#',
+    choppr: 'http://ontology.tno.nl/normengineering/choppr#',
+    co: 'http://purl.org/co/',
+    editor: 'http://ontology.tno.nl/normengineering/editor#',
+    flint: 'http://ontology.tno.nl/normengineering/flint#',
+    oa: 'http://www.w3.org/ns/oa#',
+    owl: 'http://www.w3.org/2002/07/owl#',
+    prov: 'http://www.w3.org/ns/prov#',
+    rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+    src: 'http://ontology.tno.nl/normengineering/source#',
+    xsd: 'http://www.w3.org/2001/XMLSchema#'
+}
+
     const triply = App.get({ token: token });
-    const user = await triply.getAccount("choppr");
-    const dataset = await user.getDataset("chopprdev");
-    //TODO: add the iri selected by the user.
-    const graph = await dataset.getGraph(
-     iri.iri,
-    ); // This is an example IRI, replace with the source you want to download
-   await graph.toFile("source.ttl"); // Next, convert this file to json with unwrap-api (not ready yet)
-   console.log("graph:", graph);
 
+    const user = await triply.getAccount('TNO')
+    const dataset = await user.getDataset('editor')
+    const graph = await dataset.getGraph(iri.iri)
 
-   return {statusCode: 200, body: JSON.stringify({message: "Source retreived!", source: graph})}
+    // Alternatively, get the turtle as a string
+    const store = await graph.toStore()
+    const writer = new N3.Writer({ format: 'text/turtle', prefixes })
+    store.forEach(quad => writer.addQuad(
+        quad.subject,
+        quad.predicate,
+        quad.object
+    ))
+
+    let ttlString = null;
+    // Print the output
+    writer.end((error, output) => {
+        if (error) {
+            console.error('Error exporting:', error)
+        } else {
+            // output is a string, this can be sent to unwrap-api
+            ttlString = output
+        }
+    })
+
+    if (ttlString){
+        return {
+            statusCode: 200,
+            body: JSON.stringify({message: "Source retreived!", source: ttlString})}
+    }else {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({message: "Issue with the turtle file!"})
+        }
+    }
 }
