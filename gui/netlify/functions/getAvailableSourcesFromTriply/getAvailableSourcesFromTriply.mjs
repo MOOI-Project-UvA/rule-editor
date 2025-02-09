ac2392d5cad701ff4685356d24954ceb87877366
itolios <ioannis.tolios@tno.nl>
2025-02-09 19:02:47 +0100

    testing rate limiting

--- gui/netlify/functions/getAvailableSourcesFromTriply/getAvailableSourcesFromTriply.mjs
+++ gui/netlify/functions/getAvailableSourcesFromTriply/getAvailableSourcesFromTriply.mjs
@@ -1,13 +1,25 @@
 // Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
 import SuperAgent from "superagent";
 
+const allowedDomains = [process.env.ALLOWED_DOMAINS];
+console.log("allowedDomains:", allowedDomains);
 
 export const handler = async function (event, context) {
+  const origin = event.headers["origin"];
+  console.log("origin:", origin);
+
+  if (!allowedDomains.includes(origin)) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({ error: "Forbidden" }),
+    };
+  }
+
   const token = process.env.TRIPLY_KEY;
-  const endpoint = process.env.TRIPLY_ENDPOINT
-   const reply = await SuperAgent.post(endpoint)
-    .set('Accept', 'application/sparql-results+json')
-    .set('Authorization', 'Bearer ' + token)
+  const endpoint = process.env.TRIPLY_ENDPOINT;
+  const reply = await SuperAgent.post(endpoint)
+    .set("Accept", "application/sparql-results+json")
+    .set("Authorization", "Bearer " + token)
     .buffer(true)
     .send({
       query: `
@@ -21,14 +33,24 @@ export const handler = async function (event, context) {
           src:ends ?date .
         ?editoriri rdfs:label ?editor .
       } ORDER BY DESC(?date)
-      `
+      `,
     })
-    .accept('json')
+    .accept("json");
 
-  console.log("source list", reply.body)
+  console.log("source list", reply.body);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Sources retrieved!', sources: reply.body }),
+    body: JSON.stringify({
+      message: "Sources retrieved!",
+      sources: reply.body,
+    }),
   };
+};
+
+export const config = {
+  rateLimit: {
+    windowLimit: 1,
+    aggregateBy: ["ip", "domain"],
+  },
 };
