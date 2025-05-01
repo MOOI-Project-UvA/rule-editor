c96fc6b4bd299f1c4be39f883cdd6c5d31cd41ff
Erik <erik@bloomingdata.com>
2025-05-01 10:59:36 +0200

    Create collapsable columns for source, frame list and edit

--- gui/src/views/InterpretationView.vue
+++ gui/src/views/InterpretationView.vue
@@ -1,10 +1,14 @@
 <script>
+//icons from https://pictogrammers.com/library/mdi/
 import SourceView from "./SourceView.vue";
-import FramesView from "./FramesView.vue";
+import FrameListView from "./FrameListView.vue";
+import FrameEditView from "./FrameEditView.vue"
 
 import AnnotationPanel from "../components/AnnotationPanel.vue";
 import AnnotationList from "../components/AnnotationList.vue"
 import AddingAnnotationToFramePanel from "../components/AddingAnnotationToFramePanel.vue"
+
+import { markRaw } from 'vue' //to prevent components from becoming reactie
 
 export default {
   name: "InterpretationView",
@@ -15,39 +19,53 @@ export default {
     AnnotationList,
     AddingAnnotationToFramePanel,
   },
-  computed: {
-    sourceViewIsCollapsed() {
-      return this.$store.state.sourceViewIsCollapsed
-    }
-  }
+  data: () => ({
+    panels: [
+      {
+        label: "Source",
+        component: markRaw(SourceView),
+        expanded: true,
+        icon: 'mdi-file-document-outline'
+      },
+      {
+        label: "Frames",
+        component: markRaw(FrameListView),
+        expanded: true,
+        icon: 'mdi-application-outline'
+      },
+      {
+        label: "Edit",
+        component: markRaw(FrameEditView),
+        expanded: true,
+        icon: 'mdi-pencil'
+      },
+    ]
+  }),
 };
 </script>
 
 <template>
-  <div>
-    <!-- main content of the card -->
-    <!-- source view column -->
-    <div class="row">
-      <div v-show="!sourceViewIsCollapsed" class="col-5">
-        <SourceView />
+  <div class="row q-gutter-x-xs">
+    <div v-for="panel in panels" :class="{ col: panel.expanded }">
+      <div v-if="panel.expanded" class="row items-center q-px-xs bg-primary">
+        <q-avatar text-color="white" size="lg" :icon="panel.icon" />
+        <div class="col text-white">{{ panel.label }}</div>
+        <q-btn round flat text-color="white" size="xs" icon="mdi-arrow-collapse-left"
+          @click="panel.expanded = false"></q-btn>
       </div>
-      <div class="col-auto">
-        <q-btn round size="xs" :icon="sourceViewIsCollapsed ? 'mdi-arrow-expand-right' : 'mdi-arrow-collapse-left'"
-          @click="this.$store.state.sourceViewIsCollapsed = !this.$store.state.sourceViewIsCollapsed"></q-btn>
+      <div v-else>
+        <q-btn round color="primary" size="sm" :icon="panel.icon" @click="panel.expanded = true"></q-btn>
       </div>
-      <div class="col">
-        <div>
-          <FramesView />
-        </div>
-      </div>
+      <template v-if="panel.expanded">
+        <component :is="panel.component" />
+      </template>
+
     </div>
   </div>
-  <!-- panel that appears when a new annotation is made by selecting source text -->
-  <AnnotationPanel />
-  <!-- panel that appears when existing annotation(s) have been clicked in the source text -->
-  <AnnotationList />
-  <!-- panel that appears when user is adding an annotation to a frame -->
-  <AddingAnnotationToFramePanel />
 </template>
 
-<style scoped lang="css"></style>
+<style scoped lang="css">
+.vertical {
+  writing-mode: vertical-rl;
+}
+</style>
