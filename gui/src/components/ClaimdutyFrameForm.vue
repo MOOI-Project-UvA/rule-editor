785f306a3f139e30cea8c0c290675fd62bfa52f4
itolios <ioannis.tolios@tno.nl>
2025-05-22 08:00:27 +0200

    fixed issue with v-model on commentList component

--- gui/src/components/ClaimdutyFrameForm.vue
+++ gui/src/components/ClaimdutyFrameForm.vue
@@ -8,42 +8,74 @@
             <div class="text-italic">No source added yet</div>
           </template>
           <template v-else>
-            <q-btn size="sm" flat @click="scrollToSource">Scroll to source</q-btn>
+            <q-btn size="sm" flat @click="scrollToSource"
+              >Scroll to source</q-btn
+            >
           </template>
         </div>
         <div class="col-1">
-          <q-btn size="sm" round flat color="primary" icon="mdi-comment-text-outline"
-            @click="showComments = !showComments">
-            <q-badge v-if="frame.comments.length > 0" color="primary" floating>{{ frame.comments.length }}</q-badge>
-            <q-tooltip class="text-subtitle2">
-              Comments
-            </q-tooltip>
+          <q-btn
+            size="sm"
+            round
+            flat
+            color="primary"
+            icon="mdi-comment-text-outline"
+            @click="showComments = !showComments"
+          >
+            <q-badge
+              v-if="frame.comments.length > 0"
+              color="primary"
+              floating
+              >{{ frame.comments.length }}</q-badge
+            >
+            <q-tooltip class="text-subtitle2"> Comments </q-tooltip>
           </q-btn>
         </div>
       </div>
 
-      <q-input v-model="frame.shortName" label="Short name" input-style="font-size: 12pt; font-weight:bold" />
+      <q-input
+        v-model="frame.shortName"
+        label="Short name"
+        input-style="font-size: 12pt; font-weight:bold"
+      />
       <q-input v-model="frame.fullName" label="Full name" autogrow />
 
       <div class="q-pa-md">
-        <RoleSelector :frame="frame" attribute="duty" label="Duty" :multipleFramesAllowed="false" />
-        <RoleSelector :frame="frame" attribute="claimant" label="Claimant" :multipleFramesAllowed="false" />
-        <RoleSelector :frame="frame" attribute="holder" label="Duty holder" :multipleFramesAllowed="false" />
+        <RoleSelector
+          :frame="frame"
+          attribute="duty"
+          label="Duty"
+          :multipleFramesAllowed="false"
+        />
+        <RoleSelector
+          :frame="frame"
+          attribute="claimant"
+          label="Claimant"
+          :multipleFramesAllowed="false"
+        />
+        <RoleSelector
+          :frame="frame"
+          attribute="holder"
+          label="Duty holder"
+          :multipleFramesAllowed="false"
+        />
       </div>
     </q-card-section>
     <q-card-actions align="right">
       <template v-if="frameIsBeingDeleted">
         <div class="q-mr-sm">Are you sure you want to delete this frame?</div>
-        <q-btn color="negative" @click="deleteFrame">Yes
-          <q-tooltip class="text-subtitle2">
-            Delete this frame
-          </q-tooltip>
+        <q-btn color="negative" @click="deleteFrame"
+          >Yes
+          <q-tooltip class="text-subtitle2"> Delete this frame </q-tooltip>
         </q-btn>
         <q-btn color="primary" @click="frameIsBeingDeleted = false">No</q-btn>
       </template>
       <template v-else>
-        <q-btn color="negative" @click="frameIsBeingDeleted = true">Delete</q-btn>
-        <q-btn color="primary" @click="closeFrame">Close
+        <q-btn color="negative" @click="frameIsBeingDeleted = true"
+          >Delete</q-btn
+        >
+        <q-btn color="primary" @click="closeFrame"
+          >Close
           <q-tooltip class="text-subtitle2">
             Any changes have been saved
           </q-tooltip>
@@ -53,17 +85,31 @@
     <div class="flex flex-row items-center">
       <div class="frame-id">Frame id: {{ frame.id }}</div>
       <div class="col">
-        <q-btn size="sm" round flat color="primary"
-          :icon="idIsCopiedToClipboard ? 'mdi-clipboard-check-outline' : 'mdi-clipboard-arrow-left-outline'"
-          @click="copyIdToClipboard">
+        <q-btn
+          size="sm"
+          round
+          flat
+          color="primary"
+          :icon="
+            idIsCopiedToClipboard
+              ? 'mdi-clipboard-check-outline'
+              : 'mdi-clipboard-arrow-left-outline'
+          "
+          @click="copyIdToClipboard"
+        >
           <q-tooltip class="text-subtitle2">
-            {{ idIsCopiedToClipboard ? 'Copied' : 'Copy to clipboard' }}
+            {{ idIsCopiedToClipboard ? "Copied" : "Copy to clipboard" }}
           </q-tooltip>
         </q-btn>
       </div>
     </div>
   </q-card>
-  <CommentsList :fact="frame" :showComments="showComments" @closed="showComments = false" />
+  <CommentsList
+    :fact="frame"
+    :show-comments="showComments"
+    @update:show-comments="showComments = $event"
+    @closed="showComments = false"
+  />
 </template>
 
 <script>
@@ -71,7 +117,7 @@ import RoleSelector from "./RoleSelector.vue";
 import CommentsList from "./CommentsList.vue";
 import BooleanConstructPanel from "./BooleanConstructPanel.vue";
 import SentenceList from "./SentenceList.vue";
-import { setVerticalPositionOfAnnotationLines } from "../helpers/underlining.js"
+import { setVerticalPositionOfAnnotationLines } from "../helpers/underlining.js";
 
 export default {
   emits: ["closed"],
@@ -79,50 +125,53 @@ export default {
     showSource: false,
     showComments: false,
     frameIsBeingDeleted: false, //true when user clicked delete button
-    idIsCopiedToClipboard: false
+    idIsCopiedToClipboard: false,
   }),
   computed: {
     sourceDocuments() {
-      return this.$store.state.sourceDocuments
+      return this.$store.state.sourceDocuments;
     },
     displayedSourceDocument() {
-      return this.$store.state.displayedSourceDocument
+      return this.$store.state.displayedSourceDocument;
     },
     frame() {
       return this.$store.state.frameBeingEdited;
     },
     sentences() {
-      return this.sourceDocuments.map(doc => doc.getSentencesForFrame(this.frame)).flat()
+      return this.sourceDocuments
+        .map((doc) => doc.getSentencesForFrame(this.frame))
+        .flat();
     },
   },
   methods: {
     closeFrame() {
-      this.frame.activeField = null
-      this.$store.commit("removeFrameFromEditList", this.frame)
+      this.frame.activeField = null;
+      this.$store.commit("removeFrameFromEditList", this.frame);
     },
     deleteFrame() {
-      this.frameIsBeingDeleted = null
-      this.$store.commit("removeFrame", this.frame)
-      setVerticalPositionOfAnnotationLines(this.displayedSourceDocument)
+      this.frameIsBeingDeleted = null;
+      this.$store.commit("removeFrame", this.frame);
+      setVerticalPositionOfAnnotationLines(this.displayedSourceDocument);
     },
     //scroll to source of frame, in source panel
     scrollToSource() {
       const sentenceToScrollTo = this.sentences[0];
       //show correct source
-      this.$store.state.displayedSourceDocument = sentenceToScrollTo.sourceDocument
+      this.$store.state.displayedSourceDocument =
+        sentenceToScrollTo.sourceDocument;
       //scroll to sentence
-      this.$store.state.sentenceToScrollTo = sentenceToScrollTo
+      this.$store.state.sentenceToScrollTo = sentenceToScrollTo;
     },
     copyIdToClipboard() {
       navigator.clipboard.writeText(this.frame.id);
-      this.idIsCopiedToClipboard = true
-    }
+      this.idIsCopiedToClipboard = true;
+    },
   },
   components: {
     RoleSelector,
     CommentsList,
     BooleanConstructPanel,
-    SentenceList
+    SentenceList,
   },
 };
 </script>
