d3d0ee6307196e2dd423cec708738371b3b48214
Erik <erik@bloomingdata.com>
2025-08-06 11:40:53 +0200

    Add button for removing source from interpretation

--- gui/src/views/SourceCollectionView.vue
+++ gui/src/views/SourceCollectionView.vue
@@ -1,29 +1,6 @@
 <template>
-  <!-- This component contains the content of the source collection view -->
-  <div id="source-collection-view">
+ <div id="source-collection-view">
     <q-card flat bordered class="my-card q-ma-sm" style="width: 1000px">
-      <!-- header -->
-      <!--<q-item class="q-ma-sm">
-        <q-item-section avatar>
-          <q-avatar icon="mdi-bookmark-box-multiple-outline" rounded size="xl">
-          </q-avatar>
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Collect sources</q-item-label>
-        </q-item-section>
-        <q-item-section avatar>
-          <q-avatar>
-            <q-icon name="mdi-information-outline" class="cursor-pointer"></q-icon>
-            <q-tooltip class="bg-blue-1 text-grey-10 text-body2">
-              <div style="max-width: 300px">
-                In this view, you can select legal sources either from the server or from your local file system.
-              </div>
-            </q-tooltip>
-          </q-avatar>
-        </q-item-section>
-      </q-item>
-      <q-separator />-->
-      <!--  main content of the card-->
       <q-card-section>
         <SourceLoader />
       </q-card-section>
@@ -44,10 +21,11 @@
                 <q-tab-panel v-for="(sourceDocument, docIndex) in sourceDocuments" :key="docIndex" :name="docIndex">
                   <q-card flat square>
                     <q-card-section>
-                      <q-btn color="primary" size="sm" class="q-mr-md"
+                      <q-btn color="primary" class="q-mr-md"
                         @click="sourceDocument.sentenceTree.selected = true">Select all</q-btn>
-                      <q-btn color="negative" size="sm" @click="sourceDocument.sentenceTree.selected = false">Deselect
+                      <q-btn color="negative" @click="sourceDocument.sentenceTree.selected = false">Deselect
                         all</q-btn>
+                        <q-btn class="float-right" icon="mdi-close" @click="$store.state.sourceDocuments.splice(docIndex,1)">Remove source from interpretation</q-btn>
                     </q-card-section>
                     <q-card-section class="q-pt-none expansion-items">
                       <ListComponent :sourceDocument="sourceDocument" />
@@ -59,14 +37,6 @@
           </q-splitter>
         </div>
       </div>
-      <!--  action section  -->
-      <!-- <q-card-actions class="q-pa-md" id="source-collection-view-actions">
-        <q-btn type="submit" color="primary" @click="$emit('decreaseStepper')">Back</q-btn>
-        <q-space></q-space>
-
-        <q-btn type="submit" color="primary" @click="storeSelectSources"
-          :disable="!anyCheckedSentences">Continue</q-btn>
-      </q-card-actions>-->
     </q-card>
   </div>
 </template>
